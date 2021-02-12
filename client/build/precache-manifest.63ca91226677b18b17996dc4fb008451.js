32091d3d2ea41ead6848b28c1c07ae2f6e99705d
AviadC31 <aviadcoh1@gmail.com>
2021-02-13 01:47:57 +0200

    added build directory to github repo

---
+++ client/build/precache-manifest.63ca91226677b18b17996dc4fb008451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0a2f09cacb58f4600add06df333627",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d89164f5555044c18cf5",
+    "url": "/static/css/2.20acfe37.chunk.css"
+  },
+  {
+    "revision": "a02199863ead5a0cccac",
+    "url": "/static/css/main.c8f74670.chunk.css"
+  },
+  {
+    "revision": "d89164f5555044c18cf5",
+    "url": "/static/js/2.8eda320c.chunk.js"
+  },
+  {
+    "revision": "a02199863ead5a0cccac",
+    "url": "/static/js/main.92044f06.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
